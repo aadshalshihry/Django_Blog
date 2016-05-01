d7ca45253e765d5085b1ad50a432bb3f394565fc
Roman Alshehri <aalshehri08@hotmail.com>
2016-05-01 15:07:31 -0400

    Add static files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    data = {'mydata': 'Hello World from my blog!!!!'}
+    return render(request, 'blog/index.html', data)
